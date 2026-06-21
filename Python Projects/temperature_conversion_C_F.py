8beed11e6595415b382131d80da9ef67a84a99d8
duke-of-alaska <benedictus.marcello@gmail.com>
2026-06-21 17:02:38 +0700

    Added temperature conversion program

---
+++ Python Projects/temperature_conversion_C_F.py
@@ -0,0 +1,30 @@
+# Temperature Conversion Program
+
+while True:
+    choice = input("""
+        ==== Temperature Conversion Program ====
+        What would you like to do?
+        
+        1. Convert from Celsius to Fahrenheit
+        2. Convert from Fahrenheit to Celsius
+        3. Exit
+        
+        Input (in numbers): """)
+
+    if choice == "1":
+        temperature = float(input("Enter temperature (in °C): "))
+        calculation = (temperature * 9/5) + 32
+        result = f"{calculation:.3f}°F"
+
+    elif choice == "2":
+        temperature = float(input("Enter temperature (in °F): "))
+        calculation = (temperature - 32) * 5/9
+        result = f"{calculation:.3f}°C"
+
+    elif choice == "3":
+        result = "test"
+
+    else:
+        result = "An error occurred"
+
+    print(result)

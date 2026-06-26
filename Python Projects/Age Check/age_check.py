0b27d5c01c99266f814d33e61e6e601871a3eaf8
duke-of-alaska <benedictus.marcello@gmail.com>
2026-06-26 17:04:30 +0700

    Enhanced age check program by using loops

--- Python Projects/Age Check/age_check.py
+++ Python Projects/Age Check/age_check.py
@@ -1,19 +1,27 @@
-# Age Check Program
 print("Hello!")
 
-try:
-    age = int(input("Enter your current age: "))
-    if 13 <= age < 18:
-        print("Welcome! Some restrictions have been applied to you due to your age.")
+while True:
+    try:
+        age = int(input("Enter your current age: "))
 
-    elif age >= 18:
-        print("Welcome!")
+        if age < 1 or age > 120:
+            print("That doesn't seem like a real age. Please try again.")
+            continue 
 
-    elif 0 < age < 13:
-        print("Sorry, you may not sign up yet.")
+        if 13 <= age < 18:
+            print("Welcome! Some restrictions have been applied to you due to your age.")
+            break 
 
-    else:
-        print("An error occurred.")
+        elif age >= 18:
+            print("Welcome!")
+            break 
 
-except ValueError:
-    print("Please input an integer.")
+        elif 0 < age < 13:
+            print("Sorry, you may not sign up yet.")
+            break
+
+        else:
+            print("An error occurred. Please enter a valid age.")
+
+    except ValueError:
+        print("Please input an integer.")

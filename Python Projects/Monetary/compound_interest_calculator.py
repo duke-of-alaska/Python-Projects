50071a00f797a177c24de9c54524b2e1be350416
duke-of-alaska <benedictus.marcello@gmail.com>
2026-07-01 15:53:02 +0700

    feat: implement basic compound interest calculator script

---
+++ Python Projects/Monetary/compound_interest_calculator.py
@@ -0,0 +1,10 @@
+# Compound Interest Calculator
+# A = P(1 + r/n)^nt
+
+p = float(input("Enter initial investment amount ($): "))
+r = float(input("Enter annual interest rate (eg. 0.05): "))
+n = int(input("Enter the amount of times the interest is compounded per year: "))
+t = float(input("Enter the time in years: "))
+
+a = p*(1+(r/n))**(n*t)
+print(f"${a:,.2f}")
